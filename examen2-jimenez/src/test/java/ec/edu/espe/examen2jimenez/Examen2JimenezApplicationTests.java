12730973ab6e3d4abfa22e4fdfd123929dce8c66
BRYAN ALEXANDER JIMENEZ DELGADO <bryanjd007@gmail.com>
2023-01-17 10:01:22 -0500

    git init

---
+++ examen2-jimenez/src/test/java/ec/edu/espe/examen2jimenez/Examen2JimenezApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examen2jimenez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen2JimenezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
